9c6cc68054bbc8bd25b1a237a9fa59af05ed3d08
David Yao <zhuocheng.yao@mail.utoronto.ca>
2021-11-25 09:13:49 -0500

    Build login and register page Added onclick event on customer login(would be modified later when customer database linked) Designed List Products Screen --Zhuocheng

--- app/src/main/java/com/example/fileioexample/MainActivity.java
+++ app/src/main/java/com/example/fileioexample/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.fileioexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MSG = "";
 
     //Test commit
 
@@ -12,6 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // these are for jumping into new activities after login or register
+    public void StoreRegister(View view){
+        //not updated
+    }
+    public void StoreLogin(View view){
+        //not updated
+    }
+
+    public void CustomerRegister(View view){
+        //jump to some view
+        Intent intent = new Intent(this,ListProd.class);
+        startActivity(intent);
+    }
+
+    public void CustomerLogin(View view){
+        //jump to some view
+        Intent intent = new Intent(this,ListProd.class);
+        startActivity(intent);
     }
 
 }
