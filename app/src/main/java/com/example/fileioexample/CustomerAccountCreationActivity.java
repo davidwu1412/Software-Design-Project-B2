83bc9dfe8aadfae39d622c78138e1a9423395420
Dylan <dylanwilliams2255@gmail.com>
2021-11-29 01:52:29 -0500

    Finished account creation for store owners
    Changed:
    - DatabaseUtils.java
    - OwnerAccountCreationActivity.java
    - CustomerAccountCreationActivity.java (Removed testing code)
    
    Contributor: Dylan Williams

--- app/src/main/java/com/example/fileioexample/CustomerAccountCreationActivity.java
+++ app/src/main/java/com/example/fileioexample/CustomerAccountCreationActivity.java
@@ -12,6 +12,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.fileioexample.account.CustomerAccount;
+import com.example.fileioexample.account.OwnerAccount;
 import com.example.fileioexample.utils.DatabaseUtils;
 import com.example.fileioexample.utils.Popup;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -25,18 +26,10 @@ import java.util.HashMap;
 
 public class CustomerAccountCreationActivity extends AppCompatActivity {
 
-    //Components for the alert messages for invalid inputs
-    private AlertDialog.Builder dialogBuilder;
-    private AlertDialog dialog;
-    private TextView alertMessageTextView;
-    private Button alertCancelButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_account_creation);
-
-        //databaseTest(); //Test by adding entries to the database
     }
 
     public void createCustomerAccount(View view){
@@ -46,8 +39,6 @@ public class CustomerAccountCreationActivity extends AppCompatActivity {
         EditText passwordText = (EditText) findViewById(R.id.editTextTextPassword);
         EditText passwordTextConfirm = (EditText) findViewById(R.id.editTextTextPassword2);
         String password = passwordText.getText().toString();
-
-        TextView errorText = (TextView) findViewById(R.id.textView);
 
         //Check if username text box is filled
         if(username.equals("")){
@@ -88,43 +79,6 @@ public class CustomerAccountCreationActivity extends AppCompatActivity {
         DatabaseUtils.PASSWORDS_REF.child(username).setValue(password);
         setResult(1); //Set result code to 1 to indicate account was successfully created
         finish();
-    }
-
-    public void createNewAlertPopup(String message, AppCompatActivity activity){
-
-        //Setup the dialog
-        dialogBuilder = new AlertDialog.Builder(activity);
-        final View alertPopupView = getLayoutInflater().inflate(R.layout.alert_popup, null);
-
-        alertMessageTextView = (TextView) alertPopupView.findViewById(R.id.textView13);
-        alertCancelButton = (Button) alertPopupView.findViewById(R.id.button2);
-
-        alertMessageTextView.setText(message);
-
-        //Display the alert dialog
-        dialogBuilder.setView(alertPopupView);
-        dialog = dialogBuilder.create();
-
-        int width = (int)(getResources().getDisplayMetrics().widthPixels*0.90);
-        int height = (int)(getResources().getDisplayMetrics().heightPixels*0.90);
-        dialog.getWindow().setLayout(width, height);
-
-        dialog.show();
-
-        //Setup the button functionality to dismiss the dialog
-        alertCancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.dismiss();
-            }
-        });
-    }
-
-    //This method is to be used for testing the ability to write to the database
-    public void databaseTest(){
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
-        ref.child("accounts").child("customers").child("test").setValue(new CustomerAccount("test"));
-        ref.child("accounts").child("owners").child("test2").setValue(new CustomerAccount("test2"));
     }
 
 }
--- app/src/main/java/com/example/fileioexample/utils/DatabaseUtils.java
+++ app/src/main/java/com/example/fileioexample/utils/DatabaseUtils.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.example.fileioexample.account.OwnerAccount;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
@@ -11,6 +12,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class DatabaseUtils {
 
@@ -38,6 +40,32 @@ public class DatabaseUtils {
 
     }
 
+    //This method checks if a store address is taken by an existing account in the database
+    //It returns true if the address is valid (not taken) and false if it is taken
+    public static boolean validateNewStoreAddress(String newStoreName, String newStoreAddress){
 
+        //If there are no owners, return true
+        if(DatabaseManager.owners == null)
+            return true;
+
+        for (Map.Entry entry : DatabaseManager.owners.entrySet()) {
+
+            //Get the address from each owner
+            HashMap owner = (HashMap) entry.getValue();
+            HashMap store = (HashMap) owner.get("store");
+            String storeName = (String) store.get("name");
+            String address = (String) store.get("address");
+
+            //If the address matches newStoreAddress, the address is taken, so return false
+            if(newStoreName.equals(storeName) && newStoreAddress.equals(address)){
+                Log.i("demo", "store name and address taken"); //Test
+                return false;
+            }
+        }
+
+        //Return true if the address is not found
+        return true;
+
+    }
 
 }
