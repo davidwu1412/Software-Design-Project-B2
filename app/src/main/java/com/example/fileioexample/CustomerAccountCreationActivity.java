cea7ba8006388bb63e37c55ba6fd68c3f7e5e17b
David Wu <47331647+davidwu1412@users.noreply.github.com>
2021-12-01 17:54:30 -0500

    Revert "Add login system"

--- app/src/main/java/com/example/fileioexample/CustomerAccountCreationActivity.java
+++ app/src/main/java/com/example/fileioexample/CustomerAccountCreationActivity.java
@@ -1,28 +1,8 @@
 package com.example.fileioexample;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import com.example.fileioexample.account.CustomerAccount;
-import com.example.fileioexample.account.OwnerAccount;
-import com.example.fileioexample.utils.DatabaseUtils;
-import com.example.fileioexample.utils.Popup;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class CustomerAccountCreationActivity extends AppCompatActivity {
 
@@ -31,53 +11,4 @@ public class CustomerAccountCreationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_account_creation);
     }
-
-    public void createCustomerAccount(View view){
-        EditText usernameText = (EditText) findViewById(R.id.editTextTextPersonName3);
-        String username = usernameText.getText().toString();
-
-        EditText passwordText = (EditText) findViewById(R.id.editTextTextPassword);
-        EditText passwordTextConfirm = (EditText) findViewById(R.id.editTextTextPassword2);
-        String password = passwordText.getText().toString();
-
-        //Check if username text box is filled
-        if(username.equals("")){
-            //Show a popup that tells the user to enter a username
-            Popup.createNewAlertPopup("Username field is empty", this);
-            return;
-        }
-
-        if(username.contains(" ")){
-            //Show a popup that tells the user that their username can't contain spaces
-            Popup.createNewAlertPopup("Username can't contain spaces", this);
-            return;
-        }
-
-        //Check if the passwords that the user entered match
-        if(!password.equals(passwordTextConfirm.getText().toString())){
-            //Show a popup saying that the passwords don't match
-            Popup.createNewAlertPopup("Passwords don't match", this);
-            return;
-        }
-
-        //Check if password text box is filled
-        if(password.equals("")){
-            //Show a popup that tells the user to enter a password
-            Popup.createNewAlertPopup("Password field is empty", this);
-            return;
-        }
-
-        //Check if the username is taken
-        if(!DatabaseUtils.validateUsername(username)) {
-            //Show a popup saying that the username is taken
-            Popup.createNewAlertPopup("Username is taken", this);
-            return;
-        }
-
-        //Add the new account credentials to the database
-        DatabaseUtils.addCustomerAccount(username, password);
-        setResult(1); //Set result code to 1 to indicate account was successfully created
-        finish();
-    }
-
 }
--- app/src/main/java/com/example/fileioexample/account/CustomerAccount.java
+++ app/src/main/java/com/example/fileioexample/account/CustomerAccount.java
@@ -14,31 +14,26 @@ public class CustomerAccount extends Account {
     private String lastName;
     private String email;
 
-    public CustomerAccount() {
-        super();
+    public CustomerAccount(String username, String password) {
+        super(username, password);
         this.orders = new ArrayList<Order>();
     }
 
-    public CustomerAccount(String username) {
-        super(username);
-        this.orders = new ArrayList<Order>();
-    }
-
-    public CustomerAccount(String username, ArrayList<Order> orders) {
-        super(username);
+    public CustomerAccount(String username, String password, ArrayList<Order> orders) {
+        super(username, password);
         this.orders = orders;
     }
 
-    public CustomerAccount(String username, String firstName, String lastName, String email) {
-        super(username);
+    public CustomerAccount(String username, String password, String firstName, String lastName, String email) {
+        super(username, password);
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.orders = new ArrayList<Order>();
     }
 
-    public CustomerAccount(String username, ArrayList<Order> orders, String firstName, String lastName, String email) {
-        super(username);
+    public CustomerAccount(String username, String password, ArrayList<Order> orders, String firstName, String lastName, String email) {
+        super(username, password);
         this.orders = orders;
         this.firstName = firstName;
         this.lastName = lastName;
