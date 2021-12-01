44dd1887becbfd0a7c24ea8a63e1a25d5f28f69d
Dylan <dylanwilliams2255@gmail.com>
2021-12-01 18:55:34 -0500

    Created OwnerListProductsActivity
    Added:
    - OwnerListProductsActivity.java
    
    Contributor: Dylan Williams

---
+++ app/src/main/java/com/example/fileioexample/OwnerListProductsActivity.java
@@ -0,0 +1,14 @@
+package com.example.fileioexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OwnerListProductsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_list_products);
+    }
+}
