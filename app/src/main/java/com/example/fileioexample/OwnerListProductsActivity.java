61c59aadb8595d02cd3c4f7836057554b4ab4af7
davidwu1412 <davidwu1412@gmail.com>
2021-12-01 19:51:20 -0500

    Added RecyclerView to OwnerListProductsActivity.java

--- app/src/main/java/com/example/fileioexample/OwnerListProductsActivity.java
+++ app/src/main/java/com/example/fileioexample/OwnerListProductsActivity.java
@@ -1,14 +1,19 @@
 package com.example.fileioexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class OwnerListProductsActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner_list_products);
+
+        recyclerView = findViewById(R.id.recyclerView);
     }
 }
