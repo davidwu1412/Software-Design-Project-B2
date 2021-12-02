46a8de98640f5a2a1edb2de657abfdb2f0ff4cb4
MmxMflora <flora.xie@mail.utoronto.ca>
2021-12-02 16:28:03 -0500

    Buiding Default functionality of Cart Screen

---
+++ app/src/main/java/com/example/fileioexample/Cart.java
@@ -0,0 +1,56 @@
+package com.example.fileioexample;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
+
+import Current.Current_Order;
+
+public class Cart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_origin2);
+
+        Intent intent = getIntent();
+        Current_Order Order = new Current_Order();
+
+        String Owner = Order.Owner;
+        TextView textView15 = findViewById(R.id.textView18);
+        textView15.setText("Store: "+ Owner);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId()==R.id.seeAllOrders){
+                    //NOT FINISHED
+                }
+                if(item.getItemId()==R.id.seeAllStores){
+                    //NOT FINISHED
+                }
+                if(item.getItemId()==R.id.logoutCart){
+                    Click_Logout();
+                }
+                if(item.getItemId()==R.id.uncompleteOrder){
+                    //NOT FINISHED
+                }
+                return true;
+            }
+        });
+    }
+
+    public void Click_Logout(){
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
+
+
+}
